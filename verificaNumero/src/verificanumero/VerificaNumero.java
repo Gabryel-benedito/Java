6e59f00fbd38a036eff710b7f6ee337a32c2a0bd
Gabryel Benedito Monteiro <162363126+Gabryel-benedito@users.noreply.github.com>
2025-08-05 21:27:19 -0300

    Update VerificaNumero.java

--- verificaNumero/src/verificanumero/VerificaNumero.java
+++ verificaNumero/src/verificanumero/VerificaNumero.java
@@ -1,50 +1,104 @@
-    package verificanumero;
-    import java.util.Scanner;
+<!DOCTYPE html>
+<html lang="pt-br">
 
-    public class VerificaNumero {
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula php</title>
+</head>
 
-        public static void main(String[] args) {
-            int dado, v;
-            Scanner teclado = new Scanner(System.in);
+<body>
+    <?php
+    $a = 3;
+    $b = 15;
+    $c = 7;
 
-            do{
-                v = 0;
-                System.out.println("Digite um número inteiro (digite zero para encerrar)");
-                dado = teclado.nextInt();
+    if ($a < $b) {
+        if ($a < $c) {
+            echo "A variável A possui o menor valor e vale " . $a . "<br>";
+
+            if ($b < $c) {
+                echo "A variável B possui o segundo menor valor e vale " . $b . "<br>";
+                echo "A variável C possui o maior valor e vale " . $c . "<br>";
+            } else {
+                echo "A variável C possui o segundo menor valor e vale " . $c . "<br>";
+                echo "A variável B possui o maior valor e vale " . $b . "<br>";
+            }
+        } else {
+            echo "A variável C possui o menor valor e vale " . $c . "<br>";
+            echo "A variável A possui o segundo menor valor e vale " . $a . "<br>";
+            echo "A variável B possui o maior valor e vale " . $b . "<br>";
+        }
+    } else if ($b < $c) {
+        echo "A variável B possui o menor valor e vale " . $b . "<br>";
+
+        if ($c < $a) {
+            echo "A variável C possui o segundo menor valor e vale " . $c . "<br>";
+            echo "A variável A possui o maior valor e vale " . $a . "<br>";
+        } else {
+            echo "A variável A possui o segundo menor valor e vale " . $a . "<br>";
+            echo "A variável C possui o maior valor e vale " . $c . "<br>";
+        }
+    } else {
+        echo "A variável C possui o menor valor e vale " . $c . "<br>";
+        echo "A variável B possui o segundo menor valor e vale " . $b . "<br>";
+        echo "A variável A possui o maior valor e vale " . $a . "<br>";
+    }
 
 
-                if(dado % 2 == 0){
-                    System.out.println("O número " + dado + " é par ");
 
-                }else{
-                    System.out.println("O número " + dado + " é impar ");
+    ?>
 
+
+    <?php
+
+        $a = 3;
+        $b = 15;
+        $c = 7;
+        $v = 0;
+
+
+        for ($i= $a; $i >=1 ; $i--) { 
+            if($a  % $i == 0 ){
+                if($v >= 3){
+                    echo "O número não é primo";
+
+                    $i = 0;
                 }
-
-
-
-                for(int i = dado; i >= 1; i--){
-                        
-
-                        if(dado % i == 0){
-                            v++ ; 
-
-                            if(v >= 3){
-                                System.out.println("O número não é primo");
-
-                                i = 0;
-                            }
-                        }
-                    }
-
-                
-                
-                if(v <= 2){
-                    System.out.println("O número " + dado + " é primo");
-                }
-
-
-            }while( dado != 0);
+            }
+        }
+            
+        if($v <= 2){
+            echo "<br> O número " . $a . " é primo";
         }
 
-    }
+
+         echo "<br><br> <h1>Fatorial da varaiavel C </h1><br>";
+
+        $res = 1;
+
+        for( $i = 1; $i <= 10; $i++) {
+            $res = $res * $i;
+            echo "<br> $c x $i = " . $res;
+            
+
+        }
+
+        echo "<br><br>  O fatorial de <strong>" . $c . " é " . $res  . "</strong><br>";
+    
+
+    ?>
+
+
+
+    <?php 
+
+   
+    ?>
+
+
+
+
+</body>
+
+</html>
